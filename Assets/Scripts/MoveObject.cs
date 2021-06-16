acbf73888d671620cce9183222767ad99e60ec40
Alexander Quezada <alfredq11@live.com>
2021-06-16 00:32:58 -0700

    First Commit
    Adding all files to github, so far have player object, floor and trees. The trees have a spawner that instantiate trees and  each tree destroys itself after moving to the left and hitting the -10 x  point.

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 startPostition = new Vector2(0,0);
+
+    public float xSpeed = 10.0f;
+
+    [SerializeField]
+    private float xDeadPosition = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get starting point
+        //start a list of active objects
+        //will use this list to delete objects that have reached the other sid eof the screen but also keep track of objects.
+        startPostition = (Vector2)transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveHorizontal();
+        checkPosition();
+    }
+
+    void MoveHorizontal()
+    {
+        Vector2 newPos = transform.position;
+        newPos.x += xSpeed * Time.deltaTime;
+        transform.position = newPos;
+    }
+
+    void checkPosition()
+    {
+        if(transform.position.x < xDeadPosition)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SpawnObjectHere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectHere : MonoBehaviour
+{
+
+    public GameObject goToSpawn;
+    public float pauseTime = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitToSpawn(pauseTime));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waitToSpawn(float waitTime)
+    { 
+        Instantiate(goToSpawn,transform);
+
+        yield return new WaitForSeconds(waitTime);
+
+        StartCoroutine(waitToSpawn(pauseTime));
+    }
+}
