7f127fc6e9c0ebfa1b0084aa720400f90de79249
Alexander Quezada <alfredq11@live.com>
2021-08-24 01:40:04 -0700

    Updated Animations
    I uploaded the new V2 of the sprite sheet for Brock. In the new version the wheel are seperate than the player sprites. I had to update the previous sprites and animations in order to add each of the 4 wheels and spinning animations.  Updated all previous animations. Still need to program falling animation and handle the board object after falling.

--- Assets/startScreenLogic.cs
+++ Assets/startScreenLogic.cs
@@ -29,9 +29,9 @@ public class startScreenLogic : MonoBehaviour
 
     IEnumerator waitFlash()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(0.75f);
         pressSpace.alpha = 0.0f;
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.5f);
         pressSpace.alpha = 1.0f;
         StartCoroutine(waitFlash());
         
