9ba96da6fda604038bccb7d320fcbb79edc7721f
Alexander Quezada <alfredq11@live.com>
2021-11-14 20:17:55 -0800

    Setup Player Lives
    From here will start developing lives and ads mechanic.

--- Assets/Scripts/PlayerLives.cs
+++ Assets/Scripts/PlayerLives.cs
@@ -6,17 +6,35 @@ public class PlayerLives : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public int playerLives = 0;
-    public int maxLives = 5;
+    private int playerLives = 0;
+    private int maxLives = 5;
+
+    public GameLogic gameLogic;
 
     void Start()
     {
-        
+        gameLogic = FindObjectOfType<GameLogic>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GiveLives(int numLives)
+    {
+        playerLives += numLives;
+    }
+
+    public void LooseLives(int numLivesLost)
+    {
+        playerLives -= numLivesLost;
+        if (playerLives < 0)
+        {
+            //Game over must watch add
+            //or watch extended video
+             
+        }
     }
 }
