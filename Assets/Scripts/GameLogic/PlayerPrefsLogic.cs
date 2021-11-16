87d3a666c3322ef7c33161245f49c2e028bddad9
Alexander Quezada <alfredq11@live.com>
2021-11-15 17:41:14 -0800

    Update PlayerPrefsLogic.cs
    created static strings for player pref keys. Added get and set player prefs functions for Rewarded Ads and Game Over. Created the reward ad check to see if player is within reward window.
    
    Need to create check lives and game over reset check.

--- Assets/Scripts/GameLogic/PlayerPrefsLogic.cs
+++ Assets/Scripts/GameLogic/PlayerPrefsLogic.cs
@@ -3,29 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class PlayerPrefsLogic : MonoBehaviour
 {
-    static string GO_Time_Hr_Pref = "GO_Time_Hr";
-    static string Go_Time_Min_Pref ="GO_Time_MIN";
-    static string Go_Time_Year_Pref ="GO_Time_Year";
-    static string Go_Time_DayOfYear_Pref = "GO_Time_DayOfYear";
+    public static string GO_Time_Hr_Pref = "GO_Time_Hr";
+    public static string GO_Time_Min_Pref ="GO_Time_MIN";
+    public static string GO_Time_Year_Pref ="GO_Time_Year";
+    public static string GO_Time_DayOfYear_Pref = "GO_Time_DayOfYear";
 
-    static string Player_Lives_Pref = "Player_Lives";
+    public static string Player_Lives_Pref = "Player_Lives";
 
-    static string RA_Time_Hr_Pref = "RA_Time_Hr";
-    static string RA_Time_Min_Pref = "RA_Time_MIN";
-    static string RA_Time_Year_Pref = "RA_Time_Year";
-    static string RA_Time_DayOfYear_Pref = "RA_Time_DayOfYear";
+    public static string RA_Time_Hr_Pref = "RA_Time_Hr";
+    public static string RA_Time_Min_Pref = "RA_Time_MIN";
+    public static string RA_Time_Year_Pref = "RA_Time_Year";
+    public static string RA_Time_DayOfYear_Pref = "RA_Time_DayOfYear";
+
+    bool rewardActive = false;
+    bool hasLives = false;
+
+    bool hasGO_Time_Prefs = false;
+    bool hasRA_Time_Prefs = false;
 
 
     int playerLives = 0;
 
-    int[] gameOverTimeInHMS; // [hr][min][sec]
+    int gameOverTimeHour = 0;
+    int gameOverTimeMin = 0;
     int gameOverDayOfYear = 0; // out of 365/366
     int gameOverYear = 0;
 
-    int[] rewardedAdTimeInHMS; //[hr][min][sec]
+    int rewardedAdTimeHour;
+    int rewardedAdTimeMin;
     int rewardedAdDayOfYear = 0; // out of 365
     int rewardedAdYear = 0;
 
@@ -38,18 +45,6 @@ public class PlayerPrefsLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameOverTimeInHMS = new int[3];
-        gameOverTimeInHMS[0] = 0; //hr
-        gameOverTimeInHMS[1] = 0; // min
-        gameOverTimeInHMS[2] = 0; // sec
-
-        rewardedAdTimeInHMS = new int[3];
-        rewardedAdTimeInHMS[0] = 0;
-        rewardedAdTimeInHMS[1] = 0;
-        rewardedAdTimeInHMS[2] = 0;
-
-        timeNow = System.DateTime.Now;
-
 
         //Get from playerprefs
         GetPlayerPrefs();
@@ -67,27 +62,99 @@ public class PlayerPrefsLogic : MonoBehaviour
         if (PlayerPrefs.HasKey("FirstTime"))
         {
             //not first time
-            
-
+            hasRA_Time_Prefs = GetRewardAdTimePrefs();
+            hasGO_Time_Prefs = GetGameOverTimePrefs();
         }
         else 
         {
+            int startingLives = 3;
             PlayerPrefs.SetInt("FirstTime", 0);
+            PlayerPrefs.SetInt(Player_Lives_Pref, startingLives);
+            playerLives = startingLives;
         }
     }
 
+    bool CheckRewardActive()
+    {
+        GetRewardAdTimePrefs();
+        GetTimeNow();
+
+        //check if reward ad is within one hour of watching
+        //if its been over a year, its for sure not active
+        if (rewardedAdYear == timeNow.Year)
+        {
+            //if same day
+            if (timeNow.DayOfYear == rewardedAdDayOfYear)
+            {
+                //then is now hour == reward hour+1
+                if (timeNow.Hour == rewardedAdTimeHour + 1)
+                {
+                    //if nowMin <= reward.min
+                    if (timeNow.Minute <= rewardedAdTimeMin)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+
+            //else if now.Day == reward day + 1    
+            else if (timeNow.DayOfYear == rewardedAdDayOfYear + 1)
+            {
+                //if RA=23 && now=0
+                if (timeNow.Hour == 0 && rewardedAdTimeHour == 23)
+                {
+                    //if nowMin <= reward.min
+                    if (timeNow.Minute <= rewardedAdTimeMin)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        else if (rewardedAdYear == timeNow.Year - 1)
+        {
+            if (timeNow.DayOfYear == 1 && (rewardedAdDayOfYear == 365 || rewardedAdDayOfYear == 366))
+            {
+                if (timeNow.Hour == 0 && rewardedAdTimeHour == 23)
+                {
+                    if (timeNow.Minute <= rewardedAdTimeMin)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false; 
+
+        
+    }
+    bool GetLives()
+    {
+        if (PlayerPrefs.HasKey(Player_Lives_Pref))
+        {
+            playerLives = PlayerPrefs.GetInt(Player_Lives_Pref);
+            return true;
+        }
+        else
+            return false;
+    }
+    //Get from PlayerPrefs
+    //returns false is key does not exist
     bool GetGameOverTimePrefs()
     {
-        if (PlayerPrefs.HasKey("GO_Time_HR")
-            && PlayerPrefs.HasKey("GO_Time_MIN")
-            && PlayerPrefs.HasKey("GO_Time_Year")
-            && PlayerPrefs.HasKey("GO_Time_DayOfYear"))
-        {
 
-            gameOverTimeInHMS[0] = PlayerPrefs.GetInt("GO_Time_HR"); //hr
-            gameOverTimeInHMS[1] = PlayerPrefs.GetInt("GO_Time_MIN"); // min
-            gameOverYear = PlayerPrefs.GetInt("GO_Time_Year");
-            gameOverDayOfYear = PlayerPrefs.GetInt("GO_Time_DayOfYear");
+        //Ensure the Key exists in PlayerPrefs
+        if (PlayerPrefs.HasKey(GO_Time_Hr_Pref)
+            && PlayerPrefs.HasKey(GO_Time_Min_Pref)
+            && PlayerPrefs.HasKey(GO_Time_Year_Pref)
+            && PlayerPrefs.HasKey(GO_Time_DayOfYear_Pref))
+        {
+            //Save to local variables
+            gameOverTimeHour = PlayerPrefs.GetInt(GO_Time_Hr_Pref); //hr
+            gameOverTimeMin = PlayerPrefs.GetInt(GO_Time_Min_Pref); // min
+            gameOverYear = PlayerPrefs.GetInt(GO_Time_Year_Pref);
+            gameOverDayOfYear = PlayerPrefs.GetInt(GO_Time_DayOfYear_Pref);
 
             return true;
         }
@@ -96,12 +163,52 @@ public class PlayerPrefsLogic : MonoBehaviour
         }
     }
 
+    //Save to PlayerPrefs
+    //Overwrite or create if does not exist.
     void SetGameOverTimePrefs(int hr, int min, int year, int dayOfYear)
     {
-        PlayerPrefs.SetInt("GO_Time_HR",hr); //hr
-        PlayerPrefs.SetInt("GO_Time_MIN",min); // min
-        PlayerPrefs.SetInt("GO_Time_Year",year); // year
-        PlayerPrefs.SetInt("GO_Time_DayOfYear",dayOfYear); // day of year
+        PlayerPrefs.SetInt(GO_Time_Hr_Pref,hr); //hr
+        PlayerPrefs.SetInt(GO_Time_Min_Pref,min); // min
+        PlayerPrefs.SetInt(GO_Time_Year_Pref,year); // year
+        PlayerPrefs.SetInt(GO_Time_DayOfYear_Pref,dayOfYear); // day of year
+
+        PlayerPrefs.Save();
+    }
+
+    //Get from PlayerPrefs
+    //returns false is key does not exist
+    bool GetRewardAdTimePrefs()
+    {
+        //Ensure the Key exists in PlayerPrefs
+        if (PlayerPrefs.HasKey(RA_Time_Hr_Pref)
+            && PlayerPrefs.HasKey(RA_Time_Min_Pref)
+            && PlayerPrefs.HasKey(RA_Time_Year_Pref)
+            && PlayerPrefs.HasKey(RA_Time_DayOfYear_Pref))
+        {
+            //Save to local variables
+            rewardedAdTimeHour = PlayerPrefs.GetInt(RA_Time_Hr_Pref); //hr
+            rewardedAdTimeMin = PlayerPrefs.GetInt(RA_Time_Min_Pref); // min
+            rewardedAdYear = PlayerPrefs.GetInt(RA_Time_Year_Pref);
+            rewardedAdDayOfYear = PlayerPrefs.GetInt(RA_Time_DayOfYear_Pref);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Save to PlayerPrefs
+    //Overwrite or create if does not exist.
+    void SetrewardedAdTimePrefs(int hr, int min, int year, int dayOfYear)
+    {
+        PlayerPrefs.SetInt(RA_Time_Hr_Pref, hr); //hr
+        PlayerPrefs.SetInt(RA_Time_Min_Pref, min); // min
+        PlayerPrefs.SetInt(RA_Time_Year_Pref, year); // year
+        PlayerPrefs.SetInt(RA_Time_DayOfYear_Pref, dayOfYear); // day of year
+
+        PlayerPrefs.Save();
     }
 
     void SetTimeNow()
@@ -115,40 +222,26 @@ public class PlayerPrefsLogic : MonoBehaviour
     }
     void CheckGameOverCycle()
     {
+        //Get time now
+        GetTimeNow();
 
-        //Get Now Time
-        int[] timeNowHM = new int[2];
-        int dayOfYearNow = 0;
-        int yearNow = 0;
-        System.DateTime timeNow = System.DateTime.Now;
-
-        dayOfYearNow = timeNow.DayOfYear;
-        yearNow = timeNow.Year;
-
-        timeNowHM[0] = timeNow.Hour;
-        timeNowHM[1] = timeNow.Minute;
-        //DONE Get Now Time
-
-        //Get Game Over TIme
+       
 
 
+    }
 
-        //has it been more than a year!?
-        if (yearNow <= gameOverYear)
-        {
-            //is it still today?
-            //if yes do nothing
-            //else if its tomorrow
-            //then if game Over hour < now hour?
-            //if yes then is game over min < now min?
-            //if yes then can continue without ad!
-            //
-            //
-        }
-        else 
-        {
-            // no ad required [ ]
-        }
+    void PlayedRewardedAd()
+    {
+        GetTimeNow();
 
+        SetrewardedAdTimePrefs(timeNow.Hour,timeNow.Minute,timeNow.Year,timeNow.DayOfYear);
+
+    }
+
+    void GameOverNow()
+    {
+        GetTimeNow();
+
+        SetGameOverTimePrefs(timeNow.Hour, timeNow.Minute, timeNow.Year, timeNow.DayOfYear);
     }
 }
